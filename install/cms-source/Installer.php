2706eee8ae8e88375512f9560b88657d00d3106b
WieszczY <wieszczy85@gmail.com>
2026-06-29 10:05:06 +0000

    feat: Update version to 0.2.5 and add release details for miniportal-0.2.5

--- install/cms-source/Installer.php
+++ install/cms-source/Installer.php
@@ -480,7 +480,7 @@ final class Installer
     {
         $content = json_encode([
             'installed_at' => gmdate(DATE_ATOM),
-            'version' => '0.2.4',
+            'version' => '0.2.5',
         ], JSON_THROW_ON_ERROR | JSON_UNESCAPED_SLASHES);
         if (file_put_contents($this->lockFile(), $content . PHP_EOL, LOCK_EX) === false) {
             throw new RuntimeException('Nie można zablokować instalatora po instalacji.');
